0bc723a89ba2bf07f2385543d854750bc04a9f2c
Flamex <acc@flamex.dev>
2021-06-26 01:45:27 +0200

    finished

--- website-template/src/pages/homepage.js
+++ website-template/src/pages/homepage.js
@@ -2,10 +2,16 @@ import React from 'react'
 import config from '../config.json'
 import './utils/main.css'
 
+// ASSETS
+import {ReactComponent as Discord} from '../assets/discord.svg'
+import {ReactComponent as Github} from '../assets/github.svg'
+import {ReactComponent as Twitch} from '../assets/twitch.svg'
+import {ReactComponent as Email} from '../assets/email.svg'
+
 class Card extends React.Component {
     render() {
         return (
-            <div class="card bg-gray-100 cursor-pointer transition duration-200 rounded-lg bg-gray-900 hover:shadow-xl p-5 content-around" onClick={() => window.location.href = this.props.data.url}>
+            <div class="bg-gray-100 cursor-pointer transition duration-200 ease rounded-lg bg-gray-900 hover:shadow-xl p-5 content-around" onClick={() => window.location.href = this.props.data.url}>
                 <div class="flex">
                     <img src={this.props.data.image} class="rounded-full w-10 h-10" /><span class="font-bold ml-3 mt-2">{this.props.data.name}</span>
                 </div>
@@ -13,7 +19,7 @@ class Card extends React.Component {
                     {this.props.data.description}
                 </p>
                 <p class="text-blue-300 mt-2">
-                    {this.props.data.url.split("https://").join(" ").split("hhtps://").join(" ")}
+                    {this.props.data.url.split("https://").join(" ").split("http://").join(" ")}
                 </p>
             </div>
         )
@@ -24,7 +30,7 @@ class Homepage extends React.Component {
     render() {
         return (
             <div>
-                <div className="px-40 py-20">
+                <div className="md:px-40 md:py-20 px-4 py-5">
                     <div className="flex items-center">
                         <img src={config.customization.image} className="w-40 rounded-lg mr-10" />
                         <div>
@@ -32,12 +38,27 @@ class Homepage extends React.Component {
                             <p className="text-sm font-bold text-gray-500">{config.customization.subtitle}</p>
                         </div>
                     </div>
+
+                    {config.customization.aboutme ? <>
+                        <div className="pt-20">
+                            <p className="font-bold underline">About me</p>
+                            <p className="text-gray-400 md:w-2/3" dangerouslySetInnerHTML={{__html: config.customization.aboutme}} />
+                        </div>
+                    </> : <></>}
+
                     <div id="list" className="pt-20">
-                        {config.customization.cards.reverse().map(e => <Card data={e} />)}
+                        {config.customization.cards.map(e => <Card data={e} />)}
                     </div>
                 </div>
 
-                <div className="absolute bottom-0 left-0 w-screen text-center py-3 bg-gray-900">
+                <div className="sticky bottom-0 left-0 w-screen text-center py-3 bg-gray-900">
+                    {JSON.stringify(config.customization.socials) !== "{}" ? <div className="text-left md:ml-12 ml-5 text-gray-500">Follow me on</div> : <></>}
+                    <div className="flex items-center md:px-10 px-3">
+                        {config.customization.socials.discord ? <Discord onClick={() => window.location.href = config.customization.socials.discord} className="w-10 h-10 p-2 hover:bg-blue-900 cursor-pointer transition duration-100 ease rounded-md" /> : <></>}
+                        {config.customization.socials.github ? <Github onClick={() => window.location.href = config.customization.socials.github} className="w-10 h-10 p-2 hover:bg-blue-900 cursor-pointer transition duration-100 ease rounded-md" /> : <></>}
+                        {config.customization.socials.twitch ? <Twitch onClick={() => window.location.href = config.customization.socials.twitch} className="w-10 h-10 p-2 hover:bg-blue-900 cursor-pointer transition duration-100 ease rounded-md" /> : <></>}
+                        {config.customization.socials.email ? <Email onClick={() => window.location.href = `mailto:${config.customization.socials.email}`} className="w-10 h-10 p-2 hover:bg-blue-900 cursor-pointer transition duration-100 ease rounded-md" /> : <></>}
+                    </div>
                     &copy; 2021 <a href="https://flamex.dev" className="cursor-pointer transition duration-200 hover:text-blue-500 text-blue-300">Flamex</a>
                 </div>
             </div>
