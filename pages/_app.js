dee31f532ab27258d977a68487b9b98882a13b21
Luna <71079641+Luna-devv@users.noreply.github.com>
2021-11-25 21:53:46 +0100

    create about-me section & banner

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,9 @@ export default function render({ Component, pageProps }) {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <script async src='https://arc.io/widget.min.js#3gbCMTjB'></script>
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
         <meta property="og:description" content="Hello, my name is Flamex. I am a german fullstack developer" />
         <meta property="og:title" content="Luna - Software Engineer and LGBTQ+ Activist" />
         <meta property="og:type" content="website" />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,42 +1,44 @@
-import React from 'react'
-const config = './../config.json'
+import React from 'react';
 
 import {
-  MapIcon,
-  CakeIcon,
-  CodeIcon,
-  BriefcaseIcon,
-  OfficeBuildingIcon,
-  MailIcon
+    UserIcon,
+    HomeIcon,
+    CakeIcon,
+    CodeIcon,
+    HeartIcon,
+    LockClosedIcon
 } from '@heroicons/react/outline'
 
-const cards = [
-  {
+const cards = [{
     "name": "discord-botlist.eu",
     "image": "https://cdn.discord-botlist.eu/pictures/logo.png",
     "description": "discord-botlist.eu - your Discord Bot List! We give you the availability to apply your bot and give your bot a page on our website.",
     "url": "https://discord-botlist.eu"
-  }
-]
-
-main();
-async function main() {
-  const response = await fetch('https://discord.com/api/v9/users/821472922140803112', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bot ${config.bot_token}`,
-    }
-  })
-  Console.writeLine(response)
-};
+}];
 
 export default class Homepage extends React.Component {
-  render() {
-    return (
-      <div className='main'>
-        <div className='banner'></div>
-      </div>
-    )
-  }
-}
+    render() {
+        return (
+            <div className='main'>
+                <img className='banner' src='https://cdn.xyna.space/r/f701e98771f5e36b068a92c8a8961e8e.gif' alt='banner' draggable='false'/>
+                <img className='picture' src='https://cdn.xyna.space/r/mee.jpg' alt='profile picture' draggable='false'/>
+                <text className='name'> Luna <t className='nah'>(Sia)</t> <br /></text>
+                <div className='box'>
+                    <text className='title'>about me</text> <br />
+                    <text className='description'>
+                        <LockClosedIcon className='ico' /> Shy, cautious <br />
+                        <CakeIcon className='ico' /> 17th April, 2007 <br />
+                        <HomeIcon className='ico' /> Austria, near Vienna <br />
+                        <CodeIcon className='ico' /> Hobby Software Engineer <br />
+                        <HeartIcon className='ico' /> Lesbian and LGBTQ+ Activist <br />
+                    </text>
+                </div>
+            </div>
+        );
+    };
+};
+
+async function getData() {
+    const data = await fetch(`https://api.lanyard.rest/v1/users/821472922140803112`);
+    return data;
+};
