6c42ba29ba51396c8d8c7606e7e6f96d0b26f5e4
Luna <71079641+Luna-devv@users.noreply.github.com>
2021-11-26 19:02:49 +0100

    creatre layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,29 @@
 import '../styles/globals.css'
-// import '../styles/tailwind.css'
+import '../styles/layout.css'
+import Layout from '../components/layout'
 import Head from 'next/head'
 
 export default function render({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <script async src='https://arc.io/widget.min.js#3gbCMTjB'></script>
-        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-        <meta property="og:description" content="Hello, my name is Flamex. I am a german fullstack developer" />
-        <meta property="og:title" content="Luna - Software Engineer and LGBTQ+ Activist" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://avatars.githubusercontent.com/u/71079641?v=4" />
-        <meta name="theme-color" content="#f19bec" />
-        <title>
-          Luna - Software Engineer and LGBTQ+ Activist
-        </title>
-        <link rel="shortcut icon" type="image/x-icon" href="https://avatars.githubusercontent.com/u/71079641?v=4" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
-}
+    return (
+        <>
+            <Head>
+                <meta charSet='utf-8' />
+                <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+                <script async src='https://arc.io/widget.min.js#3gbCMTjB'></script>
+                <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+                <meta property='og:description' content="Heyia, I'm Luna and I was born 14 years ago and live in Austria." />
+                <meta property='og:title' content='Luna - Software Engineer and LGBTQ+ Activist' />
+                <meta property='og:type' content='website' />
+                <meta property='og:image' content='https://avatars.githubusercontent.com/u/71079641?v=4' />
+                <meta name='theme-color' content='#f19bec' />
+                <title>
+                    Luna - Software Engineer and LGBTQ+ Activist
+                </title>
+                <link rel='shortcut icon' type='image/x-icon' href='https://avatars.githubusercontent.com/u/71079641?v=4' />
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
+};
