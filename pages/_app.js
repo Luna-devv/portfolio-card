ccd7563e58e1e752a0eda2ee703c6813405a2f49
Luna <71079641+Luna-devv@users.noreply.github.com>
2021-11-27 18:46:10 +0100

    some mobile

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
-import '../styles/layout.css'
-import Layout from '../components/layout'
-import Head from 'next/head'
+import '../styles/globals.css';
+import '../styles/layout.css';
+import '../styles/resposive.css';
+import Layout from '../components/layout';
+import Head from 'next/head';
 
 export default function render({ Component, pageProps }) {
     return (
@@ -10,7 +11,7 @@ export default function render({ Component, pageProps }) {
                 <meta charSet='utf-8' />
                 <meta name='viewport' content='width=device-width, initial-scale=1.0' />
                 <script async src='https://arc.io/widget.min.js#3gbCMTjB'></script>
-                <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+                <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
                 <meta property='og:description' content="Heyia, I'm Luna and I was born 14 years ago and live in Austria." />
                 <meta property='og:title' content='Luna - Software Engineer and LGBTQ+ Activist' />
                 <meta property='og:type' content='website' />
@@ -22,7 +23,9 @@ export default function render({ Component, pageProps }) {
                 <link rel='shortcut icon' type='image/x-icon' href='https://avatars.githubusercontent.com/u/71079641?v=4' />
             </Head>
             <Layout>
-                <Component {...pageProps} />
+                <body>
+                    <Component {...pageProps} />
+                </body>
             </Layout>
         </>
     );
---
+++ pages/go/twitter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Error404 extends React.Component {
+    componentDidMount() {
+        window.location.href = 'https://twitter.com/Luni_sia/';
+    };
+    render() {
+        return <></>
+    };
+};
