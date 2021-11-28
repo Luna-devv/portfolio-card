b7c2029a5b73ac74f0bb7d1b8179451a31405136
Luna <71079641+Luna-devv@users.noreply.github.com>
2021-11-28 22:23:54 +0100

    add service fields

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 import {
-    UserIcon,
     HomeIcon,
     CakeIcon,
     CodeIcon,
     HeartIcon,
-    LockClosedIcon
+    LockClosedIcon,
+    NewspaperIcon,
+    LinkIcon
 } from '@heroicons/react/outline'
 
 const cards = [{
-    "name": "discord-botlist.eu",
-    "image": "https://cdn.discord-botlist.eu/pictures/logo.png",
-    "description": "discord-botlist.eu - your Discord Bot List! We give you the availability to apply your bot and give your bot a page on our website.",
-    "url": "https://discord-botlist.eu"
+    name: 'discord-botlist.eu',
+    icon: 'https://cdn.discord-botlist.eu/pictures/logo.png',
+    description: 'We give you the ailability to apply and give your bot a page on our site.',
+    url: 'https://discord-botlist.eu'
+}, {
+    name: 'Tixte',
+    icon: 'https://tixte.com/static/media/logo_mark.d3b45ae2.png',
+    description: 'The fast, free and easy way to upload and store your files in the cloud.',
+    url: 'https://discord.gg/vWcEAX5G4B'
+}, {
+    name: 'Dasu',
+    icon: 'https://dasu.gifts/profile.png',
+    description: 'Utilize powerful rules and control your giveaways with no limitations.',
+    url: 'https://dasu.gifts'
 }];
 
 export default class Homepage extends React.Component {
+
     render() {
         return (
             <>
                 <div className='main'>
                 <img className='banner' src='https://cdn.xyna.space/r/f701e98771f5e36b068a92c8a8961e8e.gif' alt='banner' draggable='false'/>
                 <img className='picture' src='https://cdn.xyna.space/r/mee.jpg' alt='profile picture' draggable='false'/>
-                <text className='name'> Luna <t className='nah'>(Sia)</t> <br /></text>
+                <text className='name'> Luna <t className='nah'>she/her</t> <br /></text>
 
                 <div className='content'>
                     <div className='box'>
@@ -36,12 +49,17 @@ export default class Homepage extends React.Component {
                             <HeartIcon className='ico' /> Lesbian and LGBTQ+ Activist <br />
                         </text>
                     </div>
-
-                    {cards.map((card) => {
-                    <div className='box'>
+                    
+                    {cards.map((card) => (
+                    <div className='site-box'>
                         <text className='title'>{card.name}</text> <br />
+                        <text className='description'>
+                            <NewspaperIcon className='ico' /> {card.description} <br />
+                            <LinkIcon className='ico' /> <a className='link' href={card.url}>{card.url?.replace(`https://`, ``)?.replace(`http://`, ``)}</a> <br />
+                        </text>
+                        <img className='site-picture' src={card.icon} draggable='false'/>
                     </div>
-                    })}
+                    ))}
 
                 </div>
             </div>
