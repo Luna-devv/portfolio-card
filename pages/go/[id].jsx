71cd8ccae8731071a17b02d2d72c4b9745706e09
Luna <71079641+Luna-devv@users.noreply.github.com>
2022-01-04 17:58:02 +0100

    Update [id].jsx

--- pages/go/[id].jsx
+++ pages/go/[id].jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import config from '../../config';
 
-export default function Go({ }) {
-    return <>This redirect cannot be found</>
+export default function Go({ error }) {
+    return <>{error ? <>{JSON.stringify(error)}</> : <>This redirect cannot be found</>}</>
 };
 
 Go.getInitialProps = async ({ query, req, res }) => {
-    const link = await fetch(`${config.api}/links/${query.id}`).then(res => res.json());
+    const link = await fetch(`${config.api}/links/${query.id}`).then(res => res.json()).catch((error) => { res.end(); return { error }; });
     if (link?.content?.destination) {
         res.writeHead(307, {
             Location: link?.content?.destination
