00cd75b727b0e1251af2be43754998cc7fba4182
Luna <71079641+Luna-devv@users.noreply.github.com>
2022-01-23 15:03:37 +0100

    fix error pages

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,13 +1,22 @@
+import style from '../styles/Error.module.css';
 import React from 'react';
+import Head from 'next/head';
 
+import { HiHome } from 'react-icons/hi';
 export default class Error404 extends React.Component {
     render() {
         return (
             <>
-                <div className='error-main'>
-                    <div className='box'>
-                        <text className='title code'>404 - That's an error</text> <br />
-                        <text className='description'>The site that you wanted to visit doesn't exist..</text>
+                <Head>
+                    <title>Luna - 404</title>
+                </Head>
+                <div className={style.align}>
+                    <div className={style.container}>
+                        <strong style={{ fontSize: 32, color: '#ddd9e6' }}>404</strong><text style={{ color: 'rgb(65, 59, 74)', fontSize: 24 }}> ⌋ Page not found</text> <br />
+                        <button onClick={() => window.location.href = `/`} className='button' style={{ marginTop: 10 }}>
+                            <HiHome style={{ height: 22, width: 22, marginTop: 1.8, marginRight: 5 }} />
+                            <div>Go Home</div>
+                        </button>
                     </div>
                 </div>
             </>
