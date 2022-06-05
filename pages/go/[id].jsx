984400259a90e320772b493d26e2f1470e826536
Luna <71079641+Luna-devv@users.noreply.github.com>
2022-06-06 00:15:35 +0200

    move go page

--- pages/go/[id].jsx
+++ pages/go/[id].jsx
@@ -1,29 +1,18 @@
 import React from 'react';
 import config from '../../config';
 
-export default function Go({ link }) {
+export default function Go({ }) {
     return (
         <>
-            {JSON.stringify(link, null, 4)}
         </>
     )
 };
 
 Go.getInitialProps = async ({ query, req, res }) => {
-    const link = await fetch(`${config.api.url?.replace('whois', 'api')}/links/${query.id}`).then(res => res.json()).catch(() => { res.end(); return {}; });
+    res.writeHead(307, {
+        Location: `https://lunish.nl/${query.id}`
+    });
+    res.end();
 
-    if (link?.content?.destination) {
-        res.writeHead(307, {
-            Location: `${link?.content?.destination}${req.url.split('?')[1] ? `?${req.url.split('?')[1]}` : ''}`
-        });
-        fetch(`${config.api.url?.replace('whois', 'api')}/links/${query.id}`, {
-            method: 'PATCH',
-            headers: {
-                authorization: process.env?.token
-            }
-        });
-        res.end();
-    };
-
-    return { link };
+    return {};
 };
