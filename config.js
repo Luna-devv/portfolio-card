43e6d2f720b18bad5e40aba773421f50fac0e9b2
Luna <luni.siaa@gmail.com>
2024-06-17 22:37:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ module.exports = {
     },
     user: {
         id: "821472922140803112",
-        username: "Lunish",
+        username: "mwlica",
         discriminator: "8888",
         nickname: "Luna",
-        nickavatar: "https://cdn.waya.one/r/cat_luna.png",
+        nickavatar: "https://avatars.githubusercontent.com/u/71079641?v=4",
         status: {
             state: {
                 text: "dnd",
@@ -19,38 +19,45 @@ module.exports = {
         },
         activities: [],
         createdTimestamp: 1615924806867,
-        avatar: "https://cdn.waya.one/r/cat_luna.png",
+        avatar: "https://avatars.githubusercontent.com/u/71079641?v=4",
         banner: "https://cdn.waya.one/r/sunset.gif",
         accentColor: "#ce3760"
     },
     cards: [{
+        name: 'Wamellow',
+        icon: 'https://cdn.discordapp.com/avatars/1125449347451068437/a_986f2a6a8d2089e9cdab4f7de9c34c87.webp?size=1024',
+        description: 'Engage with /image AI, Text to Speech, and much more.',
+        url: 'https://wamellow.com?utm_source=lunish.nl',
+        position: `Founder, Developer`,
+        bot: 2
+    }, {
         name: 'Waya',
-        icon: 'https://cdn.discordapp.com/avatars/857230367350063104/a54dbb364757f99076f36003ae68f77f.png?size=1024',
+        icon: 'https://cdn.discordapp.com/avatars/857230367350063104/3873fe5ffefd4689a10bfb9a518cb2b1.webp?size=1024',
         description: 'A simple multi-function bot with Pronouns Select, Starboard and more.',
-        url: 'https://waya.one',
+        url: 'https://waya.one?utm_source=lunish.nl',
         position: `Founder, Developer`,
         bot: 2
     }, {
         name: 'Discordlist.gg',
-        icon: 'https://cdn.discordapp.com/icons/724571620676599838/37192e8b0c8981cbfd8b172a9693cc19.webp?size=2048',
+        icon: 'https://cdn.discordapp.com/icons/724571620676599838/37192e8b0c8981cbfd8b172a9693cc19.webp?size=1024',
         description: 'Find the best bots and servers to upgrade your Discord experience.',
-        url: 'https://discordlist.gg?utm_source=lunish.gay',
+        url: 'https://discordlist.gg?utm_source=lunish.nl',
         position: `Beta Contributor`
     }, {
         name: 'discord-botlist.eu',
         icon: 'https://cdn.discord-botlist.eu/pictures/logo.png',
         description: 'We give you the ability to apply and give your bot a page on our site.',
-        url: 'https://discord-botlist.eu?utm_source=lunish.gay',
+        url: 'https://discord-botlist.eu?utm_source=lunish.nl',
         position: `Moderator`
     }, {
         name: 'Tixte',
         icon: 'https://tixte.com/static/media/logo_mark.d3b45ae2.png',
         description: 'The fast, free and easy way to upload and store your files in the cloud.',
-        url: 'https://discord.gg/vWcEAX5G4B?utm_source=lunish.gay',
+        url: 'https://discord.gg/vWcEAX5G4B?utm_source=lunish.nl',
         position: `Moderator`
     }],
     header: {
-        icon: `https://cdn.waya.one/r/cat_luna.png`,
+        icon: `https://avatars.githubusercontent.com/u/71079641?v=4`,
         name: `Lunish`,
         buttons: {
             Discord: `/go/discord`,
@@ -62,7 +69,7 @@ module.exports = {
         title: `Luna - Software Engineer and LGBTQ+ Activist`,
         description: `Heyia, I'm Luna, born at 17th of April and currently live in Austria.\nI'm female and my pronouns are She/Her!`,
         image: `https://avatars.githubusercontent.com/u/71079641?v=4`,
-        domainName: `lunish.gay`,
+        domainName: `lunish.nl`,
         color: `#f19bec`,
         keywords: [
             'Luna',
